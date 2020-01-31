6104f427d8ff369f4c4c265ccd1c786a2c66ab3e
BouBooo <florent.nicolas@ynov.com>
2020-01-31 11:13:52 +0100

    orders page

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -2,19 +2,32 @@
 
 @section('content')
 
-{!! Breadcrumbs::render('orders') !!}
+<!-- Start Banner Area -->
+<section class="banner-area organic-breadcrumb">
+    <div class="container">
+        <div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
+            <div class="col-first">
+                <h1>Orders</h1>
+                <nav class="d-flex align-items-center">
+                    <a href="index.html">Home<span class="lnr lnr-arrow-right"></span></a>
+                    <a href="category.html">Orders</a>
+                </nav>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Banner Area -->
 
 <div class="container my-5">
     <div class="orders">
         <h2 class="text-center">Orders Details</h2>
-        @foreach($orders as $order)
         <div class="table-responsive order_details_table">
             <div class="d-flex justify-content-between my-5 px-5">
                 <h4>
                     <i class="fas fa-receipt"></i>
-                    Order #{{ $order->id }}
+                    Order #5412
                 </h4>
-                <h4>Date : {{ $order->created_at }}</h4>
+                <h4>Date : 22/12/2020</h4>
             </div>
             <table class="table">
                 <thead>
@@ -25,32 +38,29 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($order->products as $product)
                     <tr>
-                        <td>{{ $product->name }}</td>
-                        <td>x {{ $product->pivot->quantity }}</td>
-                        <td>$ {{ round($product->price * $product->pivot->quantity, 2) }}</td>
+                        <td>Product Name</td>
+                        <td>x 1</td>
+                        <td>$ 6</td>
                     </tr>
-                    @endforeach
                     <tr>
                         <td><b>Subtotal</b></td>
                         <td></td>
-                        <td>$ {{ round($order->paiement_subtotal, 2) }}</td>
+                        <td>$ 6</td>
                     </tr>
                     <tr>
                         <td><b>Tax</b></td>
                         <td></td>
-                        <td>$ {{ round($order->paiement_tax, 2) }}</td>
+                        <td>$ 0.85</td>
                     </tr>
                     <tr>
                         <td><b>Total</b></td>
                         <td></td>
-                        <td>$ {{ round($order->paiement_total, 2) }}</td>
+                        <td>$ 6.85</td>
                     </tr>
                 </tbody>
             </table>
         </div>
-        @endforeach
     </div>
 </div>
 @stop
